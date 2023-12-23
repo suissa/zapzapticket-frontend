ddd0f9170d417e49bd99ad39075680a12e5fc24a
Suissinha <jnascimento@gmail.com>
2023-12-23 16:34:45 -0300

    Title Component with purple bg and text white

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,8 +4,8 @@ interface TitleProps {
 
 export default function Title({ children } : TitleProps) {
     return (
-        <div className="flex flex-col justify-center">
-            <h1 className="px-5 py-2 text-2xl">
+        <div className="flex flex-col justify-center bg-purple-600 rounded">
+            <h1 className="px-5 py-2 text-2xl text-white">
                 {children}
             </h1>
             <hr className="border-2 bg-purple-500 border-purple-500" />
