6241da48d5d082172b678226e3a4103bbcaa8ea0
Suissinha <jnascimento@gmail.com>
2023-12-23 20:05:53 -0300

    Connections Hook update corrected

--- src/hooks/useConnections.ts
+++ src/hooks/useConnections.ts
@@ -6,75 +6,75 @@ import useLayout from "./useLayout"
 const API_URL = "http://localhost:9000/connections";
 
 export default function useConnections() {
-    const [ connection, setConnection ] = useState<Connection>(Connection.empty())
-    const [ connections, setConnections ] = useState<Connection[]>([])
-    const { showForm, showTable, tableVisible } = useLayout()
+  const [connection, setConnection] = useState<Connection>(Connection.empty())
+  const [connections, setConnections] = useState<Connection[]>([])
+  const { showForm, showTable, tableVisible } = useLayout()
 
-    useEffect(listConnections, [])
+  useEffect(listConnections, [])
 
-    function createConnection() {
-      setConnection(Connection.empty())
-      showForm()
-    }
+  function createConnection() {
+    setConnection(Connection.empty())
+    showForm()
+  }
 
-    function listConnections() {
-      fetch(API_URL)
-        .then(response => response.json())
-        .then(data => {
-          console.log("listConnections then", data)
-          return setConnections(data)
-        })
-    }
-
-    function getConnection(connection: Connection) {
-      setConnection(connection)
-      showForm()
-    }
-
-    async function deleteConnection(connection: Connection) {
-      listConnections()
-    }
-
-    async function saveConnection(connection: Connection) {
-      const connectionStr = JSON.stringify({
-        name: connection.name,
-        phone: connection.phone,
-        instanceName: "Teste nome conexao",
-        // instanceName: connection.instanceName,
-        instanceStatus: true,
+  function listConnections() {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => {
+        console.log("listConnections then", data)
+        return setConnections(data)
       })
-      console.log("saveConnection connectionStr", connectionStr)
-      const response = connection?.id
-        ? await fetch(`${API_URL}/${connection.id}`, {
-          method: 'PATCH',
-          headers: { 'Content-Type': 'application/json' },
-          body: connectionStr
-        })
-        : await fetch(`${API_URL}`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: connectionStr
-        });
+  }
 
-      const data = await response.json();
-    }
+  function getConnection(connection: Connection) {
+    setConnection(connection)
+    showForm()
+  }
 
-    function criarConnection() {
-        setConnection(Connection.empty())
-        showForm()
-    }
+  async function deleteConnection(connection: Connection) {
+    listConnections()
+  }
+
+  async function saveConnection(connection: Connection) {
+    const connectionStr = JSON.stringify({
+      name: connection.name,
+      phone: connection.phone,
+      instanceName: "Teste nome conexao",
+      // instanceName: connection.instanceName,
+      instanceStatus: true,
+    })
+    console.log("saveConnection connectionStr", connectionStr)
+    const response = connection?._id
+      ? await fetch(`${API_URL}/${connection._id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: connectionStr
+      })
+      : await fetch(`${API_URL}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: connectionStr
+      });
+
+    showTable()
+  }
+
+  function criarConnection() {
+    setConnection(Connection.empty())
+    showForm()
+  }
 
 
-    return {
-        connection,
-        connections,
-        createConnection,
-        saveConnection,
-        criarConnection,
-        deleteConnection,
-        getConnection,
-        listConnections,
-        showTable,
-        tableVisible
-    }
+  return {
+    connection,
+    connections,
+    createConnection,
+    saveConnection,
+    criarConnection,
+    deleteConnection,
+    getConnection,
+    listConnections,
+    showTable,
+    tableVisible
+  }
 }
