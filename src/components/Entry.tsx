6c8084dbf338e5166005f22ccdb18f35ed8d2258
Suissinha <jnascimento@gmail.com>
2023-12-22 23:23:40 -0300

    Entry select added

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,33 +1,53 @@
 import { InputHTMLAttributes } from "react"
 
 interface EntryProps extends InputHTMLAttributes<HTMLInputElement> {
-  text: string
+  text: string;
+  selectOptions?: { value: string; label: string }[];
 }
 
-export default function Entry({ text, type = 'text', ...props }: EntryProps) {
+export default function Entry({ text, type = 'text', selectOptions, ...props }: EntryProps) {
   return (
     <div className={`flex flex-col ${props.className}`}>
       <label className="mb-2">
         {text}
       </label>
       {type === 'checkbox' ? (
+        // Renderizar um input tipo checkbox
         <input
           type="checkbox"
           {...props}
           className="form-checkbox h-5 w-5 text-purple-600"
         />
+      ) : type === 'select' && selectOptions ? (
+        // Renderizar um select com as opções fornecidas
+        <select
+          {...props}
+          className={`
+            border border-purple-500 rounded-md focus:outline-none
+            bg-gray-50 px-4 py-2
+            ${props.readOnly ? '' : 'focus:bg-white'}
+          `}
+        >
+          {selectOptions.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
       ) : (
+        // Renderizar um input padrão
         <input
           type={type}
           {...props}
           className={`
-                    border border-purple-500 rounded-md focus:outline-none
-                    bg-gray-50 px-4 py-2
-                    ${props.readOnly ? '' : 'focus:bg-white'}
-                `}
+            border border-purple-500 rounded-md focus:outline-none
+            bg-gray-50 px-4 py-2
+            ${props.readOnly ? '' : 'focus:bg-white'}
+          `}
         />
       )}
     </div>
   );
 }
+
 
