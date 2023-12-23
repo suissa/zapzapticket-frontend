1e3c00861fd3826d68e7048a463ebff2fe3255c7
Suissinha <jnascimento@gmail.com>
2023-12-23 19:02:18 -0300

    Message Hook with showTable after update

--- src/hooks/useMessages.ts
+++ src/hooks/useMessages.ts
@@ -6,74 +6,82 @@ import useLayout from "./useLayout"
 const API_URL = "http://localhost:9000/messages";
 
 export default function useMessages() {
-    const [ message, setMessage ] = useState<Message>(Message.empty())
-    const [ messages, setMessages ] = useState<Message[]>([])
-    const { showForm, showTable, tableVisible } = useLayout()
+  const [message, setMessage] = useState<Message>(Message.empty())
+  const [messages, setMessages] = useState<Message[]>([])
+  const { showForm, showTable, tableVisible } = useLayout()
 
-    useEffect(listMessages, [])
+  useEffect(listMessages, [])
 
-    function createMessage() {
-      setMessage(Message.empty())
-      showForm()
-    }
+  function createMessage() {
+    setMessage(Message.empty())
+    showForm()
+  }
 
-    function listMessages() {
-      fetch(API_URL)
-        .then(response => response.json())
-        .then(data => {
-          console.log("listMessages then", data)
-          return setMessages(data)
+  function listMessages() {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => {
+        console.log("listMessages then", data)
+        return setMessages(data)
+      })
+  }
+
+  function getMessage(message: Message) {
+    setMessage(message)
+    showForm()
+  }
+
+  async function deleteMessage(message: Message) {
+    listMessages()
+  }
+
+  async function saveMessage(message: Message) {
+    console.log("saveMessage message", message)
+    const messageStr = message?._id
+      ? JSON.stringify({
+          _id: message._id,
+          title: message.title,
+          text: message.text,
+          isActive: message.isActive
         })
-    }
-
-    function getMessage(message: Message) {
-      setMessage(message)
-      showForm()
-    }
-
-    async function deleteMessage(message: Message) {
-      listMessages()
-    }
-
-    async function saveMessage(message: Message) {
-      console.log("saveMessage message", message)
-      const messageStr = JSON.stringify({
+      : JSON.stringify({
         title: message.title,
         text: message.text,
         isActive: message.isActive
       })
-      console.log("saveMessage messageStr", messageStr)
-      const response = message?.id
-        ? await fetch(`${API_URL}/${message.id}`, {
-          method: 'PATCH',
-          headers: { 'Content-Type': 'application/json' },
-          body: messageStr
-        })
-        : await fetch(`${API_URL}`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: messageStr
-        });
+    console.log("saveMessage messageStr", messageStr)
+    const response = message?._id
+      ? await fetch(`${API_URL}/${message._id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: messageStr
+      })
+      : await fetch(`${API_URL}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: messageStr
+      });
+      // listMessages()
+      showTable();
+    ;
+  }
 
-      const data = await response.json();
-    }
-
-    function criarMessage() {
-        setMessage(Message.empty())
-        showForm()
-    }
+  function criarMessage() {
+    setMessage(Message.empty())
+    showForm()
+  }
 
 
-    return {
-        message,
-        messages,
-        createMessage,
-        saveMessage,
-        criarMessage,
-        deleteMessage,
-        getMessage,
-        listMessages,
-        showTable,
-        tableVisible
-    }
+  return {
+    message,
+    messages,
+    createMessage,
+    saveMessage,
+    criarMessage,
+    deleteMessage,
+    getMessage,
+    listMessages,
+    showTable,
+    tableVisible
+  }
 }
