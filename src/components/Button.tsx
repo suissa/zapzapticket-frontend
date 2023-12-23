6b59f13f63ddcb4f61d64abcd0a015819f4d043f
Suissinha <jnascimento@gmail.com>
2023-12-23 16:09:44 -0300

    Button Component background corrected

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,18 @@
 import { ButtonHTMLAttributes } from "react"
 
-interface BotaoProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    color?: 'green' | 'blue' | 'gray'
-    children: any
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color?: 'green' | 'blue' | 'gray'
+  children: any
 }
 
-export default function Botao({ children, className, color = 'gray', ...props }: BotaoProps) {
-    return (
-        <button className={`
-            bg-gradient-to-r from-${color}-400 to-${color}-700 text-white
-            px-4 py-2 rounded-md
-            ${className}
-        `} {...props}>
-            {children}
-        </button>
-    )
+export default function Button({ children, className, color = 'gray', ...props }: ButtonProps) {
+  return (
+    <button className={`
+      bg-gradient-to-r from-blue-400 to-purple-500 text-white
+      px-4 py-2 rounded-md
+      ${className}
+    `} {...props}>
+      {children}
+    </button>
+  )
 }
