471ffc507c61461ec68f9428fd3d18ce008e2e72
Suissinha <jnascimento@gmail.com>
2023-12-27 19:41:34 -0300

    Task Component trying zindex to overlay the task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,20 +2,27 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 
 function Task(props) {
-  const { id, index, title } = props;
-  let style = {
-    backgroundColor: "purple"
-  };
+  const { id, index, title, content } = props;
+  // console.log("Task component props", props);
 
   return (
     <Draggable draggableId={id} index={index} type="TASK">
-      {provided => (
+      {(provided, snapshot) => (
         <div
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
+          style={{
+            ...provided.draggableProps.style,
+            zIndex: snapshot.isDragging ? 999999 : 'auto',
+            // Resto do seu estilo aqui
+          }}
         >
-          <h4 style={style} className="bg-gradient-to-r from-purple-500 to-purple-800 p-4 mb-2">{title}</h4>
+          <div className="p-2 mb-2 task bg-gradient-to-r from-purple-500 to-purple-800">
+
+            <h4>{title}</h4>
+            <p className="taskContent">{content}</p>
+          </div>
         </div>
       )}
     </Draggable>
