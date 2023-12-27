cbe0937f04b314ddf79795aadb5e252f500615f5
Suissinha <jnascimento@gmail.com>
2023-12-27 19:40:53 -0300

    Column Component className=kanbanColumn

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -5,26 +5,15 @@ import Task from "./Task";
 function Column(props) {
   const { droppableId, list, type } = props;
 
-  let style = {
-    backgroundColor: "rgba(255, 255, 255, .1)",
-    borderRadius: "5px",
-    padding: "10px",
-    width: "400px",
-    margin: "10px",
-    marginTop: "20px"
-  };
-
-  // console.log("type = ", droppableId, list.map(console.log));
-
   return (
     <Droppable droppableId={droppableId} type={type} key={droppableId}>
       {provided => (
-        <div {...provided.droppableProps} ref={provided.innerRef} style={style}>
+        <div {...provided.droppableProps} ref={provided.innerRef} className="kanbanColumn">
           <h2 className="mb-4">{droppableId}</h2>
 
           {list.map((val, index) => {
             return (
-              <Task id={val.id} key={val.id} index={index} title={val.title} />
+              <Task id={val.id} key={val.id} index={index} title={val.title} content={val.content} />
             );
           })}
 
