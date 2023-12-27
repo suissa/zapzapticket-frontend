e3a4d6301ccc8a7275ec4d2d996b6041dc0a4985
Suissinha <jnascimento@gmail.com>
2023-12-27 19:42:31 -0300

    useKanban Hook with correct setUpdateTaskStatus action

--- src/hooks/useKanban.ts
+++ src/hooks/useKanban.ts
@@ -5,6 +5,7 @@ import io from "socket.io-client";
 
 const socket = io("http://localhost:9000");
 const API_URL = "http://localhost:9000/contacts/messages";
+const API_URL_CONTACT = "http://localhost:9000/contacts/ticketStatus";
 
 export default function useSend() {
   const [list, setList] = useState([]);
@@ -19,9 +20,27 @@ export default function useSend() {
       })
   }
 
+  function setUpdateTaskStatus(data: any) {
+    console.log("useKanban setUpdateTaskStatus data", data)
+    // socket.emit("updateTaskStatus", data);
+    const { ticketId, ticketStatus } = data;
+    const body = { ticketStatus: ticketStatus.toLowerCase() }
+
+    fetch(`${API_URL_CONTACT}/${ticketId}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("listTickets then", data)
+        return setList(data)
+      })
+  }
   return {
     list,
     setList,
-    listTickets
+    listTickets,
+    setUpdateTaskStatus
   }
 }
