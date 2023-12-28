e1ba77a73ac322f56419f2fff6b2f4aa7806cdcc
Suissinha <jnascimento@gmail.com>
2023-12-27 21:13:14 -0300

    Group created

---
+++ src/components/FormGroup.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import Message from "../core/Message";
+import Button from "./Button";
+import Entry from "./Entry";
+
+interface FormProps {
+  message: Message
+  messageModified?: (message: Message) => void
+  canceled?: () => void
+}
+
+export default function Form({ message, canceled, messageModified }: FormProps) {
+  const [title, setTitle] = useState(message?.title ?? "")
+  const [text, setText] = useState(message?.text ?? "")
+  const [isActive, setIsActive] = useState(message?.isActive ?? "")
+  const [instanceStatus, setInstanceStatus] = useState(message?.instanceStatus ?? false)
+
+  const _id = message?._id
+  const handleSubmit = () => {
+    console.log("ID on button click:", _id); // Isso vai mostrar o ID no console
+    messageModified?.(new Message(title, text, _id));
+  }
+  return (
+    <div>
+      {_id ? (
+        <Entry
+          text="ID"
+          value={_id}
+          readOnly
+          className="mb-4"
+        />
+      ) : false}
+      <Entry
+        text="Título"
+        value={title}
+        onChange={e => setTitle((e.target as HTMLInputElement).value)}
+        className="mb-4 text-white"
+      />
+      <Entry
+        text="Texto"
+        value={text}
+        onChange={e => setText((e.target as HTMLInputElement).value)}
+        className="mb-4 text-white"
+      />
+      <Entry
+        text="isActive"
+        type="checkbox"
+        checked={true}
+        onChange={e => setIsActive(e.target.checked)}
+        className="mb-4 text-white"
+      />
+      <div className="flex justify-end mt-7">
+        <Button
+          color="blue"
+          className="mr-2"
+          onClick={handleSubmit}
+        >
+          {_id ? 'Alterar' : 'Salvar'}
+        </Button>
+        <Button onClick={canceled}>
+          Cancelar
+        </Button>
+      </div>
+    </div>
+  )
+
+}
---
+++ src/core/Group.ts
@@ -0,0 +1,38 @@
+export default class Message {
+  #_id: string
+  #title: string
+  #text: string
+  #isActive: boolean
+  #isSelected: boolean
+
+  constructor(title: string, text: string, id: string = null) {
+    this.#_id = id
+    this.#title = title
+    this.#text = text
+    this.#isActive = false
+  }
+
+  static empty() {
+    return new Message("", "")
+  }
+
+  get _id() {
+    return this.#_id
+  }
+
+  get title() {
+    return this.#title
+  }
+
+  get text() {
+    return this.#text
+  }
+
+  get isActive() {
+    return this.#isActive
+  }
+
+  get isSelected() {
+    return this.#isSelected
+  }
+}
