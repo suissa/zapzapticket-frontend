afa111d42d903b5689692f8fe7abe17510ae9cbd
Suissinha <jnascimento@gmail.com>
2023-12-31 20:00:23 -0300

    ColorPicker Component OK

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,33 +1,47 @@
-import React, { useState } from 'react';
-import { BlockPicker } from 'react-color';
+import React, { useEffect, useRef, useState } from "react";
+import { SketchPicker } from "react-color";
 
-const ColorPicker = () => {
-  const [color, setColor] = useState("#fff");
+const ColorPicker = ({ color, onChange, onOutsideClick }) => {
   const [showPicker, setShowPicker] = useState(false);
+  const ref = useRef();
 
-  const handleColorChange = (color) => {
-    setColor(color.hex);
-  };
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setShowPicker(false);
+        onOutsideClick();
+      }
+    }
+
+    // Adiciona o listener quando o componente é montado
+    document.addEventListener('mousedown', handleClickOutside);
+
+    // Limpa o listener quando o componente é desmontado
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, onOutsideClick]); // A dependência `ref` é desnecessária aqui e pode ser removida
+
 
   return (
-    <div style={{ position: 'relative' }}>
+    <div ref={ref} style={{ position: "relative" }}>
       <div 
         style={{
-          width: '30px',
-          height: '30px',
+          width: "30px",
+          height: "30px",
           backgroundColor: color,
-          cursor: 'pointer',
-          position: 'relative',
-          left: '0',
-          borderRadius: '4px'
+          cursor: "pointer",
+          position: "relative",
+          left: "0",
+          borderRadius: "4px"
         }}
         onClick={() => setShowPicker(show => !show)}
       />
       {showPicker && (
-        <div style={{ position: 'absolute', zIndex: 99999, top: '40px', left: '-70px' }}>
-          <BlockPicker
+        <div style={{ position: "absolute", zIndex: 99999, top: "40px", left: "-70px" }}>
+          <SketchPicker
             color={color}
-            onChangeComplete={handleColorChange}
+            onChangeComplete={(color) => onChange(color.hex)}
           />
         </div>
       )}
