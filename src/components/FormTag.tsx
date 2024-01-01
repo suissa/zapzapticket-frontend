3a39e7d0dd05af0e6beb981dd359153f6b003cae
Suissinha <jnascimento@gmail.com>
2023-12-31 23:00:50 -0300

    Tag Form Component OK

--- src/components/FormTag.tsx
+++ src/components/FormTag.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Tag from "../core/Tag";
 import Button from "./Button";
 import Entry from "./Entry";
@@ -7,33 +7,33 @@ import useTags from "../hooks/useTags"
 
 interface FormProps {
   tag: Tag
-  initialColor: string  // Adicione esta linha
   tagModified?: (tag: Tag) => void
   canceled?: () => void
 }
 
-export default function Form({ tag, initialColor, canceled, tagModified }: FormProps) {
-  const [text, setText] = useState(tag?.text ?? "")
-  const [name, setName] = useState(tag?.name ?? "")
-  const [selectedTag, setSelectedTag] = useState(null);
-  const [color, setColor] = useState(initialColor);
+export default function Form({ tag, canceled, tagModified }: FormProps) {
+  const [name, setName] = useState(tag?.name ?? "");
+  const [color, setColor] = useState(tag?.color ?? "#fff");
 
-  console.log("initialColor: ", initialColor)
-  const {
-    saveTag,
-  } = useTags()
+  const { saveTag } = useTags()
 
-  const _id = tag?._id
+  const _id = tag?._id;
   const handleSubmit = () => {
-    console.log("ID on button click:", _id); // Isso vai mostrar o ID no console
-    tagModified?.(new Tag(color, text, _id));
-  }
+    tagModified?.(new Tag(color, name, _id));
+  };
+
   const handleOutsideClick = (tag) => {
-    console.log("Clicou fora do color picker tag", tag);
     if (tag && tag._id) {
       saveTag({ ...tag, color: color });
     }
   };
+
+  useEffect(() => {
+    setColor(tag?.color || "#fff");
+  }, [tag]);
+
+  console.log("Cor atual:", color);
+
   return (
     <div>
       {_id ? (
@@ -43,20 +43,20 @@ export default function Form({ tag, initialColor, canceled, tagModified }: FormP
           readOnly
           className="mb-4 text-white"
         />
-      ) : false}
+      ) : null}
 
       <Entry
         text="Nome"
         value={name}
-        onChange={e => setName((e.target as HTMLInputElement).value)}
+        onChange={e => setName(e.target.value)}
         className="mb-4 text-white"
       />
-    <div className={`flex flex-col mb-4 text-white`}>
-      <label className="mb-2">
-        Cor:
-        <ColorPicker color={color} onChange={setColor} onOutsideClick={() => handleOutsideClick(tag)} />
-      </label>
-    </div>
+      <div className="flex flex-col mb-4 text-white">
+        <label className="mb-2">
+          Cor:
+          <ColorPicker color={color} onChange={setColor} onOutsideClick={() => handleOutsideClick(tag)} />
+        </label>
+      </div>
       <div className="flex justify-end mt-7">
         <Button
           color="blue"
@@ -70,6 +70,5 @@ export default function Form({ tag, initialColor, canceled, tagModified }: FormP
         </Button>
       </div>
     </div>
-  )
-
+  );
 }
