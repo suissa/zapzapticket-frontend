d942b61805f4362db4cfb89d44f8c8a99e31be65
Suissinha <jnascimento@gmail.com>
2024-01-01 03:08:57 -0300

    Tickets Page created

--- src/pages/tickets.tsx
+++ src/pages/tickets.tsx
@@ -1,46 +1,36 @@
-// import React, { useState } from "react"
-// import Message from "../components/TicketMessage"
-// import styles from "../styles/Chat.module.css"
-
-// export default function Home() {
-//     const [messages, setMessages] = useState([
-//         { id: 1, text: "Olá, como você está?", sender: "received" },
-//         { id: 2, text: "Estou bem, obrigado! E você?", sender: "sent" }
-//         // Adicione mais mensagens conforme necessário
-//     ])
-
-//     return (
-//         <div className={styles.chatContainer}>
-//             {messages.map(message => (
-//                 <Message key={message.id} text={message.text} sender={message.sender} />
-//             ))}
-//         </div>
-//     )
-// }
-
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactsList from '../components/ContactsList';
 import Chat from '../components/Chat';
 import styles from '../styles/Chat.module.css';
+import useTickets from '../hooks/useTickets';
 
 export default function Home() {
-    const [contacts, setContacts] = useState([
-        { id: 1, name: 'João', lastMessage: 'Oi, como vai?' },
-        { id: 2, name: 'Teste', lastMessage: 'Ripa' },
-        // Mais contatos...
-    ]);
-    const [selectedContact, setSelectedContact] = useState(null);
+  const [contacts, setContacts] = useState([]);
+  const [selectedContact, setSelectedContact] = useState(null);
+  const { listContacts } = useTickets();
 
-    const handleContactSelect = (contact) => {
-        setSelectedContact(contact);
-        // Aqui você pode carregar as mensagens do contato selecionado
+  useEffect(() => {
+    const fetchContacts = async () => {
+      const contactsData = await listContacts();
+      setContacts(contactsData);
     };
 
-    return (
-        <div className={styles.mainLayout}>
-            <ContactsList contacts={contacts} onContactSelect={handleContactSelect} />
-            <Chat messages={selectedContact ? selectedContact.messages : []} />
-        </div>
-    );
+    fetchContacts();
+  }, [listContacts]);
+
+  const handleContactSelect = (contact) => {
+    setSelectedContact(contact);
+    // Aqui você pode carregar as mensagens do contato selecionado
+  };
+
+  return (
+    <div className={styles.mainLayout}>
+      <ContactsList contacts={contacts} onContactSelect={handleContactSelect} />
+      <Chat messages={selectedContact ? selectedContact.messages : [{
+        id: 1, text: "Olá, como você está?", sender: "received"
+      },
+      { id: 2, text: "Bem e vc?", sender: "sent" }
+      ]} />
+    </div>
+  );
 }
