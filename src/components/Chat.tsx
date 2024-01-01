6dd44174b8819cad94095c760ed50f55d41db53d
Suissinha <jnascimento@gmail.com>
2024-01-01 20:12:31 -0300

    Chat Component with focus in the last message

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Message from "./TicketMessage";
 import styles from "../styles/Chat.module.css";
 
 const Chat = ({ messages }) => {
-  console.log({messages});
+  const endOfMessagesRef = useRef(null);
+
+  useEffect(() => {
+    // Scroll para o último elemento da mensagem
+    endOfMessagesRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div className={styles.chatContainer}>
       {messages.map((message, i) => (
         <Message key={i} text={message.text} sender={message.type} />
       ))}
+      <div ref={endOfMessagesRef} />
     </div>
   );
 };
