ed0f50b052c899bfa7af3ad40f0da4f484977e09
Suissinha <jnascimento@gmail.com>
2024-01-01 05:15:09 -0300

    Chat Component with unique key

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -6,8 +6,8 @@ const Chat = ({ messages }) => {
   console.log({messages});
   return (
     <div className={styles.chatContainer}>
-      {messages.map(message => (
-        <Message key={message.id} text={message.text} sender={message.type} />
+      {messages.map((message, i) => (
+        <Message key={i} text={message.text} sender={message.type} />
       ))}
     </div>
   );
