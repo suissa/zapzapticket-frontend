95dc753b81e77b8d61f7f01e9262e282e063310d
Suissinha <jnascimento@gmail.com>
2024-01-01 19:09:41 -0300

    TicketMessage Component with log

--- src/components/TicketMessage.tsx
+++ src/components/TicketMessage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from '../styles/Chat.module.css';
 
 const TicketMessage = ({ text, sender }) => {
+    console.log("TicketMessage: ", text, sender);
     const messageClass = sender === 'sent' ? styles.sent : styles.received;
     return <div className={`${styles.message} ${messageClass}`}>{text}</div>;
 };
