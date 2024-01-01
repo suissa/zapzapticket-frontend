2ec58be9b9b7dcb98da71f0cee2ece3c29e73193
Suissinha <jnascimento@gmail.com>
2024-01-01 03:05:18 -0300

    Icons component IconLogout added

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,6 +1,8 @@
 import { TbBrandWhatsapp } from "react-icons/tb";
-// Heroicons: https://heroicons.com/
+import { SlEye } from "react-icons/sl";
+import { AiOutlinePoweroff } from "react-icons/ai";
 
+// Heroicons: https://heroicons.com/
 
 export const IconEdit = (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -35,7 +37,7 @@ export const IconConnectionsMenu = (props) => (
 )
 
 export const IconContactsMenu = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 icon-white mt-1 ${props.className}`}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
   </svg>
 )
@@ -101,3 +103,5 @@ export const IconFastAnswers = (props) => (
 )
 
 export const IconWhatsapp = (props) => (<TbBrandWhatsapp  className={`w-6 h-6 icon-white mt-1 ${props.className}`}/>)
+export const IconShow = (props) => (<SlEye  className={`w-6 h-6 icon-white mt-1 ${props.className}`}/>)
+export const IconLogout = (props) => (<AiOutlinePoweroff  className={`w-6 h-6 icon-white mt-1 ${props.className}`}/>)
