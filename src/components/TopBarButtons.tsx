615c6b07efb2d313e44bd93f60c6bbead2ee8274
Suissinha <jnascimento@gmail.com>
2024-01-02 01:42:08 -0300

    TopBarButton Component with corret style

--- src/components/TopBarButtons.tsx
+++ src/components/TopBarButtons.tsx
@@ -5,16 +5,16 @@ const TopBar = () => {
   
   return (
     <div className={styles.topBarButtonWrapper}>
-      <div className={styles.topBarItem}>
+      <div className={styles.topBarButtonItem}>
         <button className="btn-border py-2 px-4 rounded text-purple hover:bg-purple-200">
           NOVO
         </button>
       </div>
-      <div className={`${styles.topBarItem} text-purple`}>
+      <div className={`${styles.topBarButtonItem} text-purple`}>
         TODOS
         <ToggleSwitch on={false} />
       </div>
-      <div className={styles.topBarItem}>
+      <div className={styles.topBarButtonItem}>
         <MenuDropdown />
       </div>
     </div>
