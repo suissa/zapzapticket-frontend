8e0b47fb497ea23376432d82d9e2eab58cc3c5d1
Suissinha <jnascimento@gmail.com>
2024-01-02 01:28:42 -0300

    TopBarOptions almost there

--- src/components/TopBarOptions.tsx
+++ src/components/TopBarOptions.tsx
@@ -5,12 +5,12 @@ const TopBar = () => {
   return (
     <div className={styles.topBarOptionWrapper}>
       <div className={`${styles.topBarOptionText} position-relative`}>
-        <span className={styles.topBarOptionText}>Atendendo</span>
-        <span class={`${styles.badgeNotification} absolute inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-red-500 rounded-full`}>5</span>
+        <span className={`${styles.topBarOptionText} text-purple`}>ATENDENDO</span>
+        <span className={`${styles.badgeNotification} text-purple} absolute inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-red-500 rounded-full`}>5</span>
 
       </div>
       <div className={styles.topBarOptionText}>
-        <button className={styles.topBarButton}>Em fila</button>
+        <button className={`${styles.topBarButton} text-purple`}>EM FILA</button>
       </div>
     </div>
     )
