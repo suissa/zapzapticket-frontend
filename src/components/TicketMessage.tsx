9a9bfbd2f011a78e133cd1665b0903bf5bf9d082
Suissinha <jnascimento@gmail.com>
2024-01-03 20:51:18 -0300

    TicketMessage with correct date in the message

--- src/components/TicketMessage.tsx
+++ src/components/TicketMessage.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import styles from '../styles/Ticket.module.css';
+import React from "react";
+import moment from "moment";
+import styles from "../styles/Ticket.module.css";
 
-const TicketMessage = ({ text, sender }) => {
-    // console.log("TicketMessage: ", text, sender);
-    const messageClass = sender === 'sent' ? styles.sent : styles.received;
-    return <div className={`${styles.message} ${messageClass}`}>{text}</div>;
+const TicketMessage = ({ text, sender, date }) => {
+  // console.log("TicketMessage: ", text, sender);
+  const messageClass = sender === "sent" ? styles.sent : styles.received;
+  const isoDate = date
+  const messageDate = moment(isoDate);
+  const formattedDate = messageDate.format("HH:mm");
+  console.log("TicketMessage: ", text, sender, formattedDate);
+  return <div className={`${styles.message} ${messageClass}`}>
+    <span className={styles.ticketMessageText}>
+      {text}
+    </span>
+    <span className={styles.ticketMessageDate}>
+      {formattedDate}
+    </span>
+    </div>;
 };
 
 export default TicketMessage;
