5dbf9a0c9472db24ec0e34f7f26b9f07f27c6eb0
Suissinha <jnascimento@gmail.com>
2024-01-04 15:04:05 -0300

    useTickets Hook with correct url do get the contacts

--- src/hooks/useTickets.ts
+++ src/hooks/useTickets.ts
@@ -60,7 +60,7 @@ export default function useTickets() {
 
   const listContacts = useCallback(async () => {
     try {
-      const response = await fetch(`${API_URL}/contacts/messages/last/`);
+      const response = await fetch(`${API_URL}/contacts/messages/last/40`);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
