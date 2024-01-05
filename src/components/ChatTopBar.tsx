2b178dceb2446324e6d4ed15dfbf6b12367509f3
Suissinha <jnascimento@gmail.com>
2024-01-05 00:27:38 -0300

    ChatTopBar styles OK but need actions

--- src/components/ChatTopBar.tsx
+++ src/components/ChatTopBar.tsx
@@ -9,21 +9,40 @@ export default function ChatTopBar({selectedContact}: any) {
     <>
       <div className={styles.ticketChatTopBar}>
         <div className="flex items-center justify-between">
-          <div className={`${styles.ticketChatTopBarWrapper} flex items-center`}>
+        <div className="flex items-center gap-x-2 pt-6 pl-4"> {/* Contêiner externo */}
+          <div className="w-10 h-10"> {/* Contêiner da imagem, ajuste o tamanho conforme necessário */}
+            <Image src={selectedContact.profilePictureUrl} width={40} height={40} alt={selectedContact.name} className="rounded-full" />
+          </div>
+          <div className="flex flex-col text-purple"> {/* Contêiner para Nome e Transferido para */}
+            <div className="text-lg"> {/* Estilos para o Nome */}
+              Nome: {selectedContact.name}
+            </div>
+            <div className="text-sm text-gray-600"> {/* Estilos para Transferido para */}
+              Transferido para: {selectedContact.transferedTo || "Ninguém"}
+            </div>
+          </div>
+        </div>
+
+          {/* <div className={`${styles.ticketChatTopBarWrapper} flex items-center gap-x-2`}>
             <div className={styles.ticketChatTopBarProfileImage}>
               <Image src={selectedContact.profilePictureUrl} width={40} height={40} alt={selectedContact.name} />
             </div>
-            <div className={styles.ticketChatTopBarName}>Nome: {selectedContact.name}</div>
-          </div>
+            <div className={styles.ticketChatTopBarName}>
+              Nome: {selectedContact.name}
+            </div>
+            <div>
+              Transferido para: {selectedContact.transferedTo || "Ninguém"}
+            </div>
+          </div> */}
           <div className={`${styles.ticketChatTopBarButtonsWrapper} grid grid-cols-3`}>
             <div className={`${styles.ticketChatTopBarButtonsReturn} px-4 py-2 text-purple bg-white rounded hover:bg-purple-200 focus:outline-none`}>
               <IconReturn className={`${styles.ticketChatTopBarButtonsReturnIcon} text-purple w-6 h-6 margin-0-auto`} />
             </div>
             <div className={`${styles.ticketChatTopBarButtonsAccept} px-4 py-2 text-purple bg-white rounded hover:bg-purple-200 focus:outline-none`}>
-              <IconAccept className={`${styles.ticketChatTopBarButtonsAcceptIcon} text-purple w-6 h-6 mt-1 margin-0-auto`} />
+              <IconAccept className={`${styles.ticketChatTopBarButtonsAcceptIcon} text-green w-6 h-6 mt-1 margin-0-auto`} />
             </div>
             <div className={styles.ticketChatTopBarButtonsMenu}>
-              <MenuDropdown className={styles.ticketChatTopBarButtonsMenuIcon} />
+              <MenuDropdown className={`${styles.ticketChatTopBarButtonsMenuIcon} text-purple w-6 h-6 mt-1 margin-0-auto`} />
             </div>
             {/* </button> */}
           </div>
