08d3bf1774629c1d50fdf40ef7aa1fe2cdb46ada
Suissinha <jnascimento@gmail.com>
2024-01-09 17:35:49 -0300

    Input text global css

--- src/components/EntryInput.tsx
+++ src/components/EntryInput.tsx
@@ -15,7 +15,7 @@ export default function EntryInput(props: EntryInputProps) {
         {...rest}
         className={`
           border border-purple-500 rounded-md focus:outline-none
-          bg-gray-50 px-4 py-2
+          input-form px-4 py-2
           ${props.readOnly ? "" : "focus:bg-white"}
         `}
       />
