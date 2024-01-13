5f821eff2b31b73d6822cee5cea016f71a42a505
Suissinha <jnascimento@gmail.com>
2024-01-13 02:04:59 -0300

    Plans page protected

--- src/pages/plans.tsx
+++ src/pages/plans.tsx
@@ -5,9 +5,13 @@ import Layout from "../components/Layout";
 import Table from "../components/TablePlans";
 import Menu from '../components/Menu';
 import usePlans from "../hooks/usePlans";
+import useAuth from "../hooks/useAuth";
 import useLayout from "../hooks/useLayout";
 
 export default function Home() {
+  const { isAuthenticated } = useAuth();
+  isAuthenticated();
+
   const {
     plan,
     plans,
