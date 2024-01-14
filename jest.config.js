b573f99fced4f6b84a937e00622317250ef071f0
Suissinha <jnascimento@gmail.com>
2024-01-14 19:46:39 -0300

    Jest config created

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+};
