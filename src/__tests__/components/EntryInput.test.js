15e4ef5dcca4e13f0df93fbc0cbd80d2ae4dff35
Suissinha <jnascimento@gmail.com>
2024-01-14 20:24:42 -0300

    EntryInput test

---
+++ src/__tests__/components/EntryInput.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import EntryInput from '../../components/EntryInput'; // Ajuste o caminho conforme a estrutura do seu projeto
+
+describe('EntryInput Component', () => {
+  test('renders correctly', () => {
+    render(<EntryInput text="Label Text" />);
+    const labelElement = screen.getByText('Label Text');
+    const inputElement = screen.getByRole('textbox');
+    expect(labelElement).toBeInTheDocument();
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  test('displays label text', () => {
+    render(<EntryInput text="Label Text" />);
+    const labelElement = screen.getByText('Label Text');
+    expect(labelElement).toBeInTheDocument();
+  });
+
+  test('applies readOnly prop', () => {
+    render(<EntryInput text="Label Text" readOnly />);
+    const inputElement = screen.getByRole('textbox');
+    expect(inputElement).toHaveAttribute('readOnly');
+  });
+
+  test('applies custom className', () => {
+    render(<EntryInput text="Label Text" className="custom-class" />);
+    const divElement = screen.getByLabelText('Label Text').parentElement;
+    expect(divElement).toHaveClass('custom-class');
+  });
+});
