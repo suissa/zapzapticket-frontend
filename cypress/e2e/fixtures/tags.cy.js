b9c69ea761daf5dec88425b4652a62461899d706
Suissinha <jnascimento@gmail.com>
2024-01-15 01:11:53 -0300

    tags cypress

--- cypress/e2e/fixtures/tags.cy.js
+++ cypress/e2e/fixtures/tags.cy.js
@@ -8,21 +8,17 @@ describe("Página de tags", () => {
       }
     });
 
-    // Mock da resposta da API de listagem de tarefas
     cy.intercept("GET", "http://localhost:9000/tags", {
       statusCode: 200,
       body: [
         { _id: "1", name: "Tag 1", color: "#000000" },
-        // Adicione mais tarefas mockados conforme necessário
       ]
-    }).as("getTasks");
+    }).as("getTags");
 
-    // Define o token JWT mockado no localStorage antes de visitar a página
     localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDUyODg0NTMsImV4cCI6MTcwNTI5MjA1M30.DK4dDoDEH4UO9NPrE4V5sjsy7uL_15aYaFg-hZBmxQE");
     cy.visit("http://localhost:3000/tags");
 
-    // Espera a requisição GET /api/tags ser chamada
-    cy.wait("@getTasks");
+    cy.wait("@getTags");
   });
 
   it("deve exibir uma lista de tags", () => {
