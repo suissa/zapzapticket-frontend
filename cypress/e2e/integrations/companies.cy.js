2bf3c6a54d77f040b6327e35912fdc59c195a9a3
Jean Carlo Nascimento <jnascimento@gmail.com>
2024-01-20 17:17:35 -0300

    Cypress companies

--- cypress/e2e/integrations/companies.cy.js
+++ cypress/e2e/integrations/companies.cy.js
@@ -1,45 +1,43 @@
-describe("Página de Conexões", () => {
+describe("Página de Empresas", () => {
   beforeEach(() => {
 
-    
     cy.fixture("token").then((token) => {
       console.log(token);
-      cy.intercept("POST", "http://137.184.81.207:9000/auth/login", { 
+      cy.intercept("POST", "http://137.184.81.207:9000/login", {
         statusCode: 200,
         body: {
           token: token.token
         }
       });
 
-      cy.intercept("GET", "http://137.184.81.207:9000/connections", {
+      cy.intercept("GET", "http://137.184.81.207:9000/companies", {
         statusCode: 200,
         body: [
-          { _id: "1", name: "Conexão 1", phone: "123456789", instanceName: "Conexao_1-123456789" },
+          { _id: "1", name: "Empresa 1", phone: "123456789", status: "Ativo", dueDate: "16/01/2024", recurrence: "Mensal"},
         ]
-      }).as("getConnections");
+      }).as("getCompanies");
 
       localStorage.setItem("token", token.token);
-      cy.visit("http://137.184.81.207:3000/connections");
+      cy.visit("http://137.184.81.207:3000/companies");
 
-      // Espera a requisição GET /api/connections ser chamada
-      cy.wait("@getConnections", { timeout: 10000 });
-    });
+      cy.wait("@getCompanies");
+    })
+
   });
 
-  it("deve exibir uma lista de connections", () => {
+  it("deve exibir uma lista de empresas", () => {
     cy.get("table").should("exist");
     cy.get("table tbody tr").should("have.length.at.least", 1);
   });
 
-  it("deve mudar da tabela para o formulário ao clicar em Nova Conexão", () => {
-    cy.get("button").contains("Nova Conexão").click();
-    cy.wait(1000);
+  it("deve mudar da tabela para o formulário ao clicar em Nova Empresa", () => {
+    cy.get("button").contains("Nova Empresa").click();
     cy.get("form").should("exist");
     cy.get("table").should("not.exist");
   });
 
   it("deve mudar do formulário para a tabela ao clicar em Cancelar", () => {
-    cy.get("button").contains("Nova Conexão").click();
+    cy.get("button").contains("Nova Empresa").click();
     cy.get("form").should("exist");
     cy.get("table").should("not.exist");
     cy.get("button").contains("Cancelar").click();
