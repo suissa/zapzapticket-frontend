702702aae4e66aed87c72af49d502a95209e64d1
Jean Carlo Nascimento <jnascimento@gmail.com>
2024-01-20 19:51:21 -0300

    Cypress plans

--- cypress/e2e/integrations/plans.cy.js
+++ cypress/e2e/integrations/plans.cy.js
@@ -1,16 +1,26 @@
+// const ENV = "development"
+const ENV = "production"
+const BASE_URL_DEV = "http://localhost"
+const BASE_URL_PROD = "http://137.184.81.207"
+const BASE_URL = ENV == "development" ? BASE_URL_DEV : BASE_URL_PROD
+const LOGIN_URL = `${BASE_URL}:9000/login`
+const API_URL = `${BASE_URL}:9000/plans/active`
+const TEST_URL = `${BASE_URL}:3000/plans`
+const BUTTON = "Nova Mensagem"
+
 describe("Página de Planos", () => {
   beforeEach(() => {
 
     cy.fixture("token").then((token) => {
       console.log(token);
-      cy.intercept("POST", "http://137.184.81.207:9000/login", {
+      cy.intercept("POST", `${LOGIN_URL}`, {
         statusCode: 200,
         body: {
           token: token.token
         }
       });
 
-      cy.intercept("GET", "http://137.184.81.207:9000/plans/active", {
+      cy.intercept("GET", `${API_URL}`, {
         statusCode: 200,
         body: [
           { _id: "1", name: "Plano 1", users: 1, connections: 1, queues: 1, value: 1},
@@ -18,7 +28,7 @@ describe("Página de Planos", () => {
       }).as("getPlans");
 
       localStorage.setItem("token", token.token);
-      cy.visit("http://137.184.81.207:3000/plans");
+      cy.visit(`${TEST_URL}`);
 
       // cy.wait("@getPlans", {timeout: 20000});
     })
@@ -30,14 +40,14 @@ describe("Página de Planos", () => {
     cy.get("table tbody tr").should("have.length.at.least", 1);
   });
 
-  it("deve mudar da tabela para o formulário ao clicar em Novo Plano", () => {
-    cy.get("button").contains("Novo Plano").click();
+  it(`deve mudar da tabela para o formulário ao clicar em ${BUTTON}`, () => {
+    cy.get("button").contains(BUTTON).click();
     cy.get("form").should("exist");
     cy.get("table").should("not.exist");
   });
 
   it("deve mudar do formulário para a tabela ao clicar em Cancelar", () => {
-    cy.get("button").contains("Novo Plano").click();
+    cy.get("button").contains(BUTTON).click();
     cy.get("form").should("exist");
     cy.get("table").should("not.exist");
     cy.get("button").contains("Cancelar").click();
